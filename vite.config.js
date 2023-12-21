04ad238e622a9fbb3abbe1b380fde10f898693af
Maureen Hsu <maureen.hsu@gmail.com>
2023-12-21 22:55:46 +0800

    ViteRestart

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import ViteRestart from 'vite-plugin-restart'
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    ViteRestart({
+      restart: [
+        'my.config.[jt]s',
+      ]
+    })
+  ],
 })
