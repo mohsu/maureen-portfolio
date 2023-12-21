bbbd65ec6a5bfccce230cf7a9da1d3b0e2e70059
Maureen Hsu <maureen.hsu@gmail.com>
2023-12-21 23:58:25 +0800

    rm testimoials

--- src/components/Feedbacks.jsx
+++ src/components/Feedbacks.jsx
@@ -3,7 +3,6 @@ import { motion } from "framer-motion";
 import { styles } from "../styles";
 import { SectionWrapper } from "../hoc";
 import { fadeIn, textVariant } from "../utils/motion";
-import { testimonials } from "../constants";
 
 const FeedbackCard = ({
   index,
